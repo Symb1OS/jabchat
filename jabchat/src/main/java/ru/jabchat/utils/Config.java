e54286b1878e2753bf56116467ba1f74e1872b57
Symb1OS <symbi0s@mail.ru>
2015-10-22 13:03:22 +0300

    Update Config.java

--- jabchat/src/main/java/ru/jabchat/utils/Config.java
+++ jabchat/src/main/java/ru/jabchat/utils/Config.java
@@ -7,10 +7,10 @@ import org.apache.commons.dbcp.BasicDataSource;
 
 public class Config {
 
-	private static final String DRIVER_NAME   = "com.ibm.db2.jcc.DB2Driver";
-	private static final String URL_DATA_BASE = "jdbc:db2://fsrumosdp0001:50000/FINANZ";
-	private static final String USERNAME 	  = "srv_abx";
-	private static final String PASSWORD   	  = "PFvPiFR@";
+	private static final String DRIVER_NAME   = "";
+	private static final String URL_DATA_BASE = "";
+	private static final String USERNAME 	  = "";
+	private static final String PASSWORD   	  = "@";
 
 	public static BasicDataSource getDataSource() {
 		BasicDataSource ds = new BasicDataSource();
@@ -24,14 +24,14 @@ public class Config {
 	public static void setProxy() {
 
     	System.setProperty("java.net.useSystemProxies", "true");
-        System.setProperty("http.proxyHost", 	 	    "10.41.77.151");
-        System.setProperty("http.proxyPort",  			"8080");
-        System.setProperty("http.proxyUser", 	 		"dkx60pi");
-        System.setProperty("http.proxyPassword", 		"699GuH691");
+        System.setProperty("http.proxyHost", 	 	    "");
+        System.setProperty("http.proxyPort",  			"");
+        System.setProperty("http.proxyUser", 	 		"");
+        System.setProperty("http.proxyPassword", 		"");
 
 		Authenticator.setDefault(new Authenticator() {
 			public PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("dkx60pi", "699GuH691".toCharArray());
+				return new PasswordAuthentication("", "".toCharArray());
 			}
 		});
         
